5dea1293ae6759e31815f6daaa71273832886048
mathandhigrowth <alltechtamilblogger@gmail.com>
2025-07-19 09:25:42 +0530

    updates

--- insta-frontend/src/config/envConfig.js
+++ insta-frontend/src/config/envConfig.js
@@ -1,4 +1,4 @@
-export const BACKEND_API_URL = "https://insta-backend-automation-178521a811be.herokuapp.com/auth/callback";
+export const BACKEND_API_URL = "https://insta-backend-automation-178521a811be.herokuapp.com";
 
 export function assertEnv() {
   if (!BACKEND_API_URL) {
